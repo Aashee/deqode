f9f0523b47c044beae3afb24743bacefd8722429
Aashee Garg <aashee@systango.com>
2021-06-15 23:13:04 +0530

    test2 added

---
+++ test2.js
@@ -0,0 +1,13 @@
+let a = 5;
+if(true){
+    let a = 7;
+    console.log('aaaa', a);
+}
+console.log('aaaanext', a);
+
+var a = 5;
+if(true){
+    var a = 7;
+    console.log('aaaa', a);
+}
+console.log('aaaanext', a);
